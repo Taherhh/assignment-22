7c91d696972702e74245befc677fcb117cce0976
Taher Shabbir <taherihh53@gmail.com>
2024-06-12 19:30:28 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// assignment 22
+// Intentional Error: if you haven't recieved an array index error in one of your programs yet,try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program
+var intentionError = [1, 2, 3, 4, 5];
+console.log(intentionError[10]);
+var intentionErrors = [1, 2, 3, 4, 5];
+console.log(intentionErrors[2]);
